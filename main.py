ec1901772b6a718d895dfb52c27c0f245ce8de44
Cesar <134793116+cchaaya@users.noreply.github.com>
2023-07-22 00:01:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
+"""Load html from files, clean up, split, ingest into Weaviate."""
+import pickle
+
+from langchain.document_loaders import ReadTheDocsLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+
+#  OpenAI API key
+OPENAI_API_KEY = "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"
+
+
+def ingest_docs():
+    """Get documents from web pages."""
+    loader = ReadTheDocsLoader("langchain.readthedocs.io/en/latest/")
+    raw_documents = loader.load()
+    print(raw_documents)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    documents = text_splitter.split_documents(raw_documents)
+    print("Documents:", documents)
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+   
+
+    vectorstore = FAISS.from_documents(documents, embeddings)
+
+    # Save vectorstore
+    with open("vectorstore.pkl", "wb") as f:
+        pickle.dump(vectorstore, f)
+
+
+if __name__ == "__main__":
+    ingest_docs()
+
+#end of ingestion file
+
 """Main entrypoint for the app."""
 import logging
 import pickle
