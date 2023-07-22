4ac41b2984bcf63a1a7928cb43eb19e319ef956c
Cesar <134793116+cchaaya@users.noreply.github.com>
2023-07-22 11:49:30 +0300

    Update query_data.py

--- query_data.py
+++ query_data.py
@@ -8,6 +8,7 @@ from langchain.chains.llm import LLMChain
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.vectorstores.base import VectorStore
+from query_data import get_chain
 
 
 def get_chain(
